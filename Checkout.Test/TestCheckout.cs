a611a2599480bc3aeff1d00021c9bdf58ad46ee2
mattheusmgermano <mattheus.mgermano@gmail.com>
2023-10-05 13:23:19 +0100

    Add project files.

---
+++ Checkout.Test/TestCheckout.cs
@@ -0,0 +1,25 @@
+namespace Checkout.Test
+{
+    public class TestCheckout
+    {
+        [Fact]
+        public void TestCheckoutTotalPrice()
+        {
+            var pricingRules = new Dictionary<string, PricingRule>
+            {
+                ["A"] = new PricingRule(50, 3, 130),
+                ["B"] = new PricingRule(30, 2, 45),
+                ["C"] = new PricingRule(20),
+                ["D"] = new PricingRule(15)
+            };
+
+            var checkout = new Checkout(pricingRules);
+            checkout.Scan("B");
+            checkout.Scan("A");
+            checkout.Scan("B");
+
+            Assert.Equal(95, checkout.GetTotalPrice());
+        }
+
+    }
+}
